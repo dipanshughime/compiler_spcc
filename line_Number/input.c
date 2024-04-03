de96bab57eda0d15e111002f24f5fd4d0eb16ff1
Dipanshu Ghime <dipanshughime@gmail.com>
2024-04-03 23:53:18 +0530

    line number

--- line_Number/input.c
+++ line_Number/input.c
@@ -1,6 +1,4 @@
-
 #include <stdio.h>
-
 int main() {
     int num;
     printf("Enter a number: ");
---
+++ line_Number/output.c
@@ -0,0 +1,8 @@
+Line 1: #include <stdio.h>
+Line 2: int main() {
+Line 3:     int num;
+Line 4:     printf("Enter a number: ");
+Line 5:     scanf("%d", &num);
+Line 6:     printf("You entered: %d\n", num);
+Line 7:     return 0;
+Line 8: }
